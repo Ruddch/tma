d212c3e7598d9dafdcb136838375fed039e15e02
Semion Shishlov <semionshish@gmail.com>
2024-09-25 17:07:27 +0200

    norm

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
+import Logo from './assets/ava.png'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
 
 function App() {
   const [count, setCount] = useState(0)
 
+    useEffect(() => {
+        if (count > 5) {
+            WebApp.showAlert(`Hello! Ты должен уже ${count} btc`)
+        }
+    }, [count])
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='ava'>
+          <img src={Logo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Таро аниме бот</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+         Гадать
         </button>
         <p>
-         Таро аниме бот
+            Вы должны {count} btc
         </p>
       </div>
-        <div className="card">
-            <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-                Show Alert
-            </button>
-        </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
