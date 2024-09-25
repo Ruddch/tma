af9d70da8143bcf272f1c68014a4944d813f4397
Semion Shishlov <semionshish@gmail.com>
2024-09-25 16:32:46 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+         Таро аниме бот
         </p>
       </div>
         <div className="card">
